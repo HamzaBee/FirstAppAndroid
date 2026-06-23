74c44af25a4c0d77c3b93b4a8d470f58d73f0af4
HamzaBee <hamzabenbrahim6060@gmail.com>
2026-06-23 12:06:07 +0100

    Implement Euro to DH conversion app with history list

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.test
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editTextAmount = findViewById<EditText>(R.id.editTextAmount)
+        val btnCompute = findViewById<Button>(R.id.btnCompute)
+        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+        val listViewResult = findViewById<ListView>(R.id.listViewResults)
+
+        val data = ArrayList<String>()
+        val stringArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        listViewResult.adapter = stringArrayAdapter
+
+        btnCompute.setOnClickListener {
+            val amountText = editTextAmount.text.toString()
+            if (amountText.isNotEmpty()) {
+                try {
+                    val amount = amountText.toDouble()
+                    val result = amount * 11
+                    textViewResult.text = result.toString()
+                    data.add("$amount=>$result")
+                    stringArrayAdapter.notifyDataSetChanged()
+                    editTextAmount.setText("")
+                } catch (e: NumberFormatException) {
+                    // Handle invalid input format gracefully
+                }
+            }
+        }
+    }
+}
